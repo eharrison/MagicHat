c2171224422b8fe1824c529925ea867aad3f01b4
Evandro Harrison Hoffmann <evandro@itsdayoff.com>
2017-12-07 23:13:50 +0800

    Added hat and balls.

--- MagicHat/ViewController.swift
+++ MagicHat/ViewController.swift
@@ -13,21 +13,17 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet weak var magicButton: UIButton!
+    @IBOutlet weak var throwBallButton: UIButton!
+    
+    private var hatNode: SCNNode?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set the view's delegate
         sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+        sceneView.scene = SCNScene()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,6 +31,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -47,21 +44,116 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
-    // MARK: - ARSCNViewDelegate
+    // MARK: - Events
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
+    @IBAction func magicButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func throwBallButtonPressed(_ sender: Any) {
+        if let ballNode = createBallFromScene() {
+            ballNode.physicsBody?.applyForce(SCNVector3Make(0, 0, -3), asImpulse: true)
+            sceneView.scene.rootNode.addChildNode(ballNode)
+        }
+    }
+    
+    @IBAction func didTap(_ sender: UITapGestureRecognizer) {
+        guard hatNode == nil else {
+            return
+        }
+        
+        let location = sender.location(in: sceneView)
+        let results = sceneView.hitTest(location, types: .existingPlaneUsingExtent)
+        if let result = results.first {
+            placeHat(result)
+        }
+    }
+    
+    // MARK: - Hat
+    
+    private func placeHat(_ result: ARHitTestResult) {
+        
+        // Get transform of result
+        let transform = result.worldTransform
+        
+        // Get position from transform (4th column of transformation matrix)
+        let planePosition = SCNVector3Make(transform.columns.3.x, transform.columns.3.y, transform.columns.3.z)
+        
+        // can only add one hat
+        hatNode?.removeFromParentNode()
+        hatNode = createHatFromScene(planePosition)
+        if let hatNode = hatNode {
+            sceneView.scene.rootNode.addChildNode(hatNode)
+        }
+    }
+    
+    private func createHatFromScene(_ position: SCNVector3) -> SCNNode? {
+        guard let url = Bundle.main.url(forResource: "art.scnassets/hat", withExtension: "scn") else {
+            NSLog("Could not find hat scene")
+            return nil
+        }
+        guard let node = SCNReferenceNode(url: url) else { return nil }
+        
+        node.load()
+        
+        // Position scene
+        node.position = position
+        
         return node
     }
-*/
+    
+    // MARK: - Ball
+    
+    private func createBallFromScene() -> SCNNode? {
+        guard let url = Bundle.main.url(forResource: "art.scnassets/ball", withExtension: "scn") else {
+            NSLog("Could not find hat scene")
+            return nil
+        }
+        guard let node = SCNReferenceNode(url: url) else { return nil }
+        
+        node.load()
+        
+        // Position scene
+        node.position = getZForward(node: node)
+        
+        return node
+    }
+    
+    func getZForward(node: SCNNode) -> SCNVector3 {
+        return SCNVector3(node.worldTransform.m31, node.worldTransform.m32, node.worldTransform.m33)
+    }
+    
+    // MARK: - ARSCNViewDelegate
+    
+    private var planeNode: SCNNode?
+    
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        // Create an SCNNode for a detect ARPlaneAnchor
+        guard let _ = anchor as? ARPlaneAnchor else {
+            return nil
+        }
+        planeNode = SCNNode()
+        return planeNode
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        // Create an SNCPlane on the ARPlane
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {
+            return
+        }
+        
+        let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+        
+        let planeMaterial = SCNMaterial()
+        planeMaterial.diffuse.contents = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 0.3)
+        plane.materials = [planeMaterial]
+        
+        let planeNode = SCNNode(geometry: plane)
+        planeNode.position = SCNVector3Make(planeAnchor.center.x, 0, planeAnchor.center.z)
+        
+        planeNode.transform = SCNMatrix4MakeRotation(-Float.pi / 2, 1, 0, 0)
+        
+        node.addChildNode(planeNode)
+    }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
